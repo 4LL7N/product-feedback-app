ad612046b89506a3c69fda39a41a00a3f287c883
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-04-05 00:05:39 -0400

    style: adjust text colors

--- src/page/NewFeedback.tsx
+++ src/page/NewFeedback.tsx
@@ -4,25 +4,24 @@ import { Form } from "react-router-dom";
 import { useSubmit } from "react-router-dom";
 
 const NewFeedback: React.FC = () => {
-  const { value, handleSubmit } = useSubmit("initial Value");
+  // const { value, handleSubmit } = useSubmit("initial Value");
 
   const handleClick = () => {
-    handleSubmit("New Value");
+    // handleSubmit("New Value");
   };
 
   return (
     <InputField className="w-96 sm:w-450 md:w-550 h-screen mx-auto items-center p-10">
-      <div className="flex flex-row gap-5">
+      <div className="flex flex-row gap-4 ">
         <img
           src="/assets/shared/icon-arrow-left.svg"
           alt="icon-arrow-left"
-          // FIXME:
-          className="flex-none h-3 w-2"
+          className="my-auto"
         />{" "}
-        <span>Go Back</span>
+        <span className="">Go Back</span>
       </div>
 
-      <WhiteContainer className="relative border-spacing-2 py-10 px-5 mt-10 flex flex-col gap-5">
+      <WhiteContainer className="relative border-spacing-2 py-10 px-5 mt-12 flex flex-col gap-5">
         <img
           className="absolute top-0 left-10 -translate-x-1/2 -translate-y-1/2 w-10 "
           src="/assets/shared/icon-new-feedback.svg"
@@ -41,13 +40,13 @@ const NewFeedback: React.FC = () => {
         <div className="">
           <label htmlFor=""> Category</label>
           <p>Choose a category for your feedback</p>
-          <StyledSelect name="sort" id="">
+          <select name="sort" id="" className="text-xs text-indigo-900">
             <option value="">Feature</option>
             <option value="">UI</option>
             <option value="">UX</option>
             <option value="">Enhancement</option>
             <option value="">Bug</option>
-          </StyledSelect>
+          </select>
         </div>
 
         <div className="">
@@ -72,7 +71,6 @@ export default NewFeedback;
 
 const InputField = styled.div`
   background-color: #f7f8fd;
-
   font-family: Jost;
 
   span {
@@ -82,11 +80,6 @@ const InputField = styled.div`
     font-size: 13px;
     color: #647196;
   }
-`;
-
-const StyledSelect = styled.select`
-  font-size: 13px;
-  color: #3a4374;
 `;
 
 const WhiteContainer = styled.div`
