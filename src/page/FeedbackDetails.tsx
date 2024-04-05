26f0919ec59f6e8d263eed982ad0cdcf77d18eee
nika <nika3141535@gmail.com>
2024-04-05 18:12:44 +0400

    feat:add commets replies

--- src/page/FeedbackDetails.tsx
+++ src/page/FeedbackDetails.tsx
@@ -102,31 +102,90 @@ function FeedbackDetails() {
               </div>
             </div>
           </div>
-          <div className="rounded-[10px] p-[24px] bg-[#ffffff] mt-[24px] " >
-            <h2 className="text-[#3a4374] text-[18px] font-bold " >{feedback?.comments?.length} {feedback?.comments?.length && feedback?.comments?.length > 1? "Comments":"Comment"} </h2>
-            {feedback?.comments?.map((item:Comment,index:number) => {
-                
-              return(
+          <div className="rounded-[10px] p-[24px] bg-[#ffffff] mt-[24px] ">
+            <h2 className="text-[#3a4374] text-[18px] font-bold ">
+              {feedback?.comments?.length}{" "}
+              {feedback?.comments?.length && feedback?.comments?.length > 1
+                ? "Comments"
+                : "Comment"}{" "}
+            </h2>
+            {feedback?.comments?.map((item: Comment, index: number) => {
+              console.log(index, feedback?.comments?.length);
+
+              return (
                 <>
-                  <div className=" mt-[24px] pb-[24px] border-b border-b-solid border-b-[#8c92b39d] " >
-                    <div className="flex items-center justify-between w-[100%] " >
-                      <div className="flex gap-[16px] " >
-                      <img className="w-[40px] h-[40px] rounded-[50%] " src={item.user.image} alt="" />
-                      <div>
-                        <h3 className="text-[#3a4374] text-[13px] font-bold " >{item.user.name}</h3>
-                        <p className="text-[#647196] text-[13px] " >@{item.user.username}</p>
+                  <div
+                    className={` mt-[24px] pb-[24px] ${
+                      index + 1 === feedback?.comments?.length
+                        ? ""
+                        : "border-b border-b-solid border-b-[#8c92b338]"
+                    } `}
+                  >
+                    <div className="flex items-center justify-between w-[100%] ">
+                      <div className="flex gap-[16px] ">
+                        <img
+                          className="w-[40px] h-[40px] rounded-[50%] "
+                          src={item.user.image}
+                          alt=""
+                        />
+                        <div>
+                          <h3 className="text-[#3a4374] text-[13px] font-bold ">
+                            {item.user.name}
+                          </h3>
+                          <p className="text-[#647196] text-[13px] ">
+                            @{item.user.username}
+                          </p>
+                        </div>
                       </div>
-                      </div>
-                      <p className="text-[#4661e6] text-[13px] font-semibold " >Reply</p>
+                      <p className="text-[#4661e6] text-[13px] font-semibold ">
+                        Reply
+                      </p>
                     </div>
-                    <div className="mt-[16px]" >
-                      <p className="text-[13px] text-[#647196] " >{item.content}</p>
+                    <div className="mt-[16px]">
+                      <p className="text-[13px] text-[#647196] ">
+                        {item.content}
+                      </p>
+                    </div>
+                    <div className={`${item.replies?"flex":""} `}>
+                      <div className={item.replies?` bg-[#8c92b338] w-[1px] h-[216px] mt-[24px]`:"hidden"} />
+                      <div  className="ml-[23px] "> 
+                      {item.replies?.map((items, index) => {
+                        return (
+                          <>
+                            <div className="flex items-center justify-between w-[100%]  mt-[24px]">
+                              <div className="flex gap-[16px] ">
+                                <img
+                                  className="w-[40px] h-[40px] rounded-[50%] "
+                                  src={items.user.image}
+                                  alt=""
+                                />
+                                <div>
+                                  <h3 className="text-[#3a4374] text-[13px] font-bold ">
+                                    {items.user.name}
+                                  </h3>
+                                  <p className="text-[#647196] text-[13px] ">
+                                    @{items.user.username}
+                                  </p>
+                                </div>
+                              </div>
+                              <p className="text-[#4661e6] text-[13px] font-semibold ">
+                                Reply
+                              </p>
+                            </div>
+                            <div className="mt-[16px]">
+                              <p className="text-[13px] text-[#647196] ">
+                              <a className="text-[13px] text-[#ad1fea] font-bold " >@{items.user.username} </a>{item.content}
+                              </p>
+                            </div>
+                          </>
+                        );
+                      })}
+                      </div>
                     </div>
                   </div>
                 </>
-              )
+              );
             })}
-
           </div>
         </section>
       </main>
