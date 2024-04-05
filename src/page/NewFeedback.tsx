0e142e2bfb391573efe6298ffbad9788803dfd43
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-04-04 23:50:27 -0400

    style: add few styled components for inputs

--- src/page/NewFeedback.tsx
+++ src/page/NewFeedback.tsx
@@ -11,44 +11,59 @@ const NewFeedback: React.FC = () => {
   };
 
   return (
-    <InputField className="border-2">
-      <img
-        src="team-1/public/assets/shared/icon-arrow-left.svg"
-        alt="icon-arrow-left"
-      />{" "}
-      go back
-      <div className="relative border-spacing-2">
+    <InputField className="w-96 sm:w-450 md:w-550 h-screen mx-auto items-center p-10">
+      <div className="flex flex-row gap-5">
         <img
-          className="absolute"
-          src="team-1/public/assets/shared/icon-new-feedback.svg"
+          src="/assets/shared/icon-arrow-left.svg"
+          alt="icon-arrow-left"
+          // FIXME:
+          className="flex-none h-3 w-2"
+        />{" "}
+        <span>Go Back</span>
+      </div>
+
+      <WhiteContainer className="relative border-spacing-2 py-10 px-5 mt-10 flex flex-col gap-5">
+        <img
+          className="absolute top-0 left-10 -translate-x-1/2 -translate-y-1/2 w-10 "
+          src="/assets/shared/icon-new-feedback.svg"
           alt="icon-new-feedback"
         />
-        <h1>Create New Feedback</h1>
-        <form method="post" action="/events">
-          <label htmlFor=""> Feedback Title</label>
-          <p>Add a short, descriptive headline</p>
-          <input type="text" name="title" />
-        </form>
-        <form method="get" action=""></form>
-        <label htmlFor=""> Category</label>
-        <p>Choose acategory for your feedback</p>
-        <select name="sort" id="">
-          <option value="">Feature</option>
-          <option value="">UI</option>
-          <option value="">UX</option>
-          <option value="">Enhancement</option>
-          <option value="">Bug</option>
-        </select>
-        <form method="post" action="/events">
-          <label htmlFor=""> Feedback Detail</label>
-          <p>
-            Include any specific comments on what should be improved, added,
-            etc.
-          </p>
-          <input type="text" name="detail" />
-        </form>
+        <h1 className="">Create New Feedback</h1>
+
+        <div className="">
+          <form method="post" action="/events">
+            <label htmlFor=""> Feedback Title</label>
+            <p>Add a short, descriptive headline</p>
+            <input type="text" name="title" />
+          </form>
+        </div>
+
+        <div className="">
+          <label htmlFor=""> Category</label>
+          <p>Choose a category for your feedback</p>
+          <StyledSelect name="sort" id="">
+            <option value="">Feature</option>
+            <option value="">UI</option>
+            <option value="">UX</option>
+            <option value="">Enhancement</option>
+            <option value="">Bug</option>
+          </StyledSelect>
+        </div>
+
+        <div className="">
+          <form method="post" action="/events">
+            <label htmlFor=""> Feedback Detail</label>
+            <p>
+              Include any specific comments on what should be improved, added,
+              etc.
+            </p>
+            <input type="text" name="detail" className="last-child" />
+          </form>
+        </div>
+
         <button onClick={handleClick}> Add Feedback</button>
-      </div>
+        <button>Cancel</button>
+      </WhiteContainer>
     </InputField>
   );
 };
@@ -56,8 +71,62 @@ const NewFeedback: React.FC = () => {
 export default NewFeedback;
 
 const InputField = styled.div`
+  background-color: #f7f8fd;
+
+  font-family: Jost;
+
+  span {
+    color: #647196;
+  }
   p {
     font-size: 13px;
     color: #647196;
+  }
+`;
+
+const StyledSelect = styled.select`
+  font-size: 13px;
+  color: #3a4374;
+`;
+
+const WhiteContainer = styled.div`
+  background-color: #fff;
+  border-radius: 15px;
+
+  h1 {
+    font-size: 18px;
+    font-weight: bold;
+    color: #3a4374;
+    letter-spacing: -0.25px;
+  }
+  label {
+    font-size: 13px;
+    font-weight: bold;
+    color: #3a4374;
+  }
+  input,
+  select {
+    width: 100%;
+    height: 48px;
+    flex-grow: 0;
+    margin: 16px 0 0;
+    border-radius: 5px;
+    background-color: #f7f8fd;
+  }
+  .last-child {
+    height: 100px;
+  }
+  button {
+    margin-top: 25px;
+    padding: 10.5px 32.8px 10.5px 37.5px;
+    border-radius: 10px;
+    background-color: #ad1fea;
+    &:last-child {
+      background-color: #3a4374;
+      margin-top: 0px;
+    }
+    font-size: 13px;
+    font-weight: bold;
+    color: #f2f4fe;
   }
 `;
