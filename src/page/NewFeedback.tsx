54348ad43ed243027ef17c4dd204f8ea4da03c92
Giorgi Nareklishvili <38813634+gionare@users.noreply.github.com>
2024-04-06 13:28:38 -0400

    feat: responsive design

--- src/page/NewFeedback.tsx
+++ src/page/NewFeedback.tsx
@@ -29,17 +29,17 @@ const NewFeedback: React.FC = () => {
   console.log(watch("title")); // watch input value by passing the name of it
 
   return (
-    <InputField className="w-96 sm:w-450 md:w-550 h-full mx-auto items-center p-10">
+    <InputField className="w-96 sm:w-450 h-full mx-auto items-center p-10">
       <div className="flex flex-row gap-4 ">
         <img
           src="/assets/shared/icon-arrow-left.svg"
           alt="icon-arrow-left"
           className="my-auto"
         />{" "}
-        <span className="">Go Back</span>
+        <span className="goback">Go Back</span>
       </div>
 
-      <WhiteContainer className="relative border-spacing-2 py-10 px-5 mt-12 flex flex-col gap-5">
+      <WhiteContainer className="border-2 relative border-spacing-2 py-10 px-5 mt-12 flex flex-col gap-5">
         <img
           className="absolute top-0 left-10 -translate-x-1/2 -translate-y-1/2 w-10 "
           src="/assets/shared/icon-new-feedback.svg"
@@ -94,7 +94,7 @@ const NewFeedback: React.FC = () => {
             {/* include validation with required or other standard HTML validation rules */}
             <input
               className="last-child"
-              placeholder="describe feedback"
+              placeholder="min 4 letters"
               {...register("description", {
                 required: true,
                 maxLength: 200,
@@ -105,10 +105,12 @@ const NewFeedback: React.FC = () => {
               })}
             />
             {/* errors will return when field validation fails  */}
-            {errors.description && <span>This field is required</span>}
+            {errors.description && (
+              <span className="tex-red-800">Can't be empty</span>
+            )}
           </div>
 
-          <div className="flex flex-col gap-2">
+          <div className="buttons flex flex-col gap-2">
             <button type="submit">Add Feedback</button>
             <button>Cancel</button>
           </div>
@@ -125,11 +127,24 @@ const InputField = styled.div`
   font-family: Jost;
 
   span {
+    color: #d73737;
+  }
+  .goback {
     color: #647196;
   }
   p {
     font-size: 13px;
     color: #647196;
+  }
+
+  @media (min-width: 600px) {
+    width: 540px;
+
+    .buttons {
+      flex-direction: row-reverse;
+      align-items: end;
+      justify-content: end;
+    }
   }
 `;
 
